eaf98e12596426cf7ad94ec8f68eddcf2e074e53
SelimBayındır <SelimBayndr@SELIMBAYINDIR>
2025-02-26 15:19:30 +0300

    CQRS

--- Core/MovieApi.Application/Features/CQRSDesignPattern/Queries/MovieQueries/GetMovieById.cs
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Queries/MovieQueries/GetMovieById.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Application.Features.CQRSDesignPattern.Queries.MovieQueries
 {
-    internal class GetMovieById
+    public class GetMovieById
     {
+
     }
 }
